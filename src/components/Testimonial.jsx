00d4234d913a73a19f31214381c655db99227c54
Moideen Mashad <moideenmashad@gmail.com>
2023-07-05 13:11:06 +0530

    Adde css file to testimonial designs

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Testimonial = () => {
+  return (
+    // Testimonial
+    <div>
+      <div className="container-fluid">
+        <div className="container">
+          <div className="row g-4">
+            <center className="">
+              <h4 className="mt-4 mb-4">What our customers says</h4>
+            </center>
+            <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+              <div className="card">
+                <p className="p-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Explicabo quisquam quidem laborum nemo reiciendis aspernatur
+                  molestias, accusantium pariatur repudiandae consequuntur enim
+                  amet aut voluptas id ex quis nulla dolorem quaerat.
+                </p>
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div className="card">
+                <p className="p-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Explicabo quisquam quidem laborum nemo reiciendis aspernatur
+                  molestias, accusantium pariatur repudiandae consequuntur enim
+                  amet aut voluptas id ex quis nulla dolorem quaerat.
+                </p>
+              </div>
+            </div>
+            <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div className="card">
+                <p className="p-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Explicabo quisquam quidem laborum nemo reiciendis aspernatur
+                  molestias, accusantium pariatur repudiandae consequuntur enim
+                  amet aut voluptas id ex quis nulla dolorem quaerat.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
