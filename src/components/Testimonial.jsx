3f97454ac7b2e2f987ddd13a8dc50ddb43e44534
Moideen Mashad <moideenmashad@gmail.com>
2023-07-07 20:04:26 +0530

    added bg-img to image folder>public

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -8,49 +8,69 @@ const Testimonial = () => {
         <div className="container">
           <div className="row g-4">
             <center className="mb-5">
-              <h2 className="mt-4 mb-5 text-light fw-bold text-dark">WHAT CLIENTS SAY</h2>
+              <h2 className="mt-4 mb-5 text-light fw-bold text-dark">WHAT OUR<span id="clients"> CUSTOMERS SAY</span> </h2>
             </center>
-            <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
               <div className="card mb-5">
               <center>
                 <img src="\Img\p1.png" alt="" srcset="" className="img-fluid profile-img mb-3"/><br />
-                <strong className="">Unknown</strong>
+                <strong className="mb-1">Unknown</strong><br />
+                <div className="star-row">
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                </div>
+                
               </center>
                 <p className="p-3">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  "Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Explicabo quisquam quidem laborum nemo reiciendis aspernatur
                   molestias, accusantium pariatur repudiandae consequuntur enim
-                  amet aut voluptas id ex quis nulla dolorem quaerat.
+                  amet aut voluptas id ex quis nulla dolorem quaerat."
                 </p>
               </div>
             </div>
-            <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
             <div className="card mb-5 ">
             <center>
                 <img src="\Img\p3.png" alt="" srcset="" className="img-fluid profile-img mb-3"/><br />
-                <strong className="">Unknown</strong>
+                <strong className="mb-1">Unknown</strong><br />
+                <div className="star-row">
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                </div>
               </center>
                 <p className="p-3">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  "Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Explicabo quisquam quidem laborum nemo reiciendis aspernatur
                   molestias, accusantium pariatur repudiandae consequuntur enim
-                  amet aut voluptas id ex quis nulla dolorem quaerat.
+                  amet aut voluptas id ex quis nulla dolorem quaerat."
     
                 </p>
               </div>
             </div>
-            <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
             <div className="card mb-5">
               <center>
                 <img src="\Img\p2.jpg" alt="" srcset="" className="img-fluid profile-img mb-3"/><br />
-                <strong className="">Unknown</strong>
+                <strong className="mb-1">Unknown</strong>
+                <div className="star-row">
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                  <img src="\Img\star.png" alt="" srcset="" className="star" />
+                </div>
               </center>
                 
                 <p className="p-3">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  "Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Explicabo quisquam quidem laborum nemo reiciendis aspernatur
                   molestias, accusantium pariatur repudiandae consequuntur enim
-                  amet aut voluptas id ex quis nulla dolorem quaerat.
+                  amet aut voluptas id ex quis nulla dolorem quaerat."
                 </p>
               </div>
             </div>
